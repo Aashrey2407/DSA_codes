0056b830533096bb4308f7dca14ce89f11ec2afe
Aashrey2407 <aashreyrachaputi@gmail.com>
2025-02-11 07:50:46 +0530

    find maximum in all windows

---
+++ SLIDING_WINDOW/SlidingWindowMax.cpp
@@ -0,0 +1,30 @@
+//the link to the question is: https://www.interviewbit.com/problems/sliding-window-maximum/
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> Solution::slidingMaximum(const vector<int> &A, int B) {
+    int n = A.size();
+    int start = 0;
+    int end = 0;
+    vector<int> answer;
+    priority_queue<pair<int,int>> maxHeap;
+    map<pair<int,int>,int> mpp;
+    while(end<n){
+        maxHeap.push({A[end],end});
+        if(end-start+1==B){
+            pair<int,int> topElement = maxHeap.top();
+            while(!maxHeap.empty() && mpp.find(maxHeap.top())!=mpp.end()){
+                maxHeap.pop();
+            }
+            answer.push_back(maxHeap.top().first);
+            if(maxHeap.top().first==A[start] && maxHeap.top().second==start){
+                maxHeap.pop();
+            }
+            else{
+                mpp[{A[start],start}]++;
+            }
+            start++;
+        }
+        end++;
+    }
+    return answer;
+}
