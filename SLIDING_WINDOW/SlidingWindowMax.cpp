73ace54ae38780e5a9a695af8e0cfbcf5025ce69
Aashrey2407 <aashreyrachaputi@gmail.com>
2025-02-11 11:22:56 +0530

    sliding window maximum

--- SLIDING_WINDOW/SlidingWindowMax.cpp
+++ SLIDING_WINDOW/SlidingWindowMax.cpp
@@ -1,7 +1,7 @@
 //the link to the question is: https://www.interviewbit.com/problems/sliding-window-maximum/
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> Solution::slidingMaximum(const vector<int> &A, int B) {
+vector<int> slidingMaximum(const vector<int> &A, int B) {
     int n = A.size();
     int start = 0;
     int end = 0;
